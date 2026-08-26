2d9b0d10048f442175f5cbcec60141219fd94835
Jay <44783760+jschenke488@users.noreply.github.com>
2026-08-26 09:18:26 -0500

    add try catch to command registration

--- src/main/java/net/zytolga/Jerry.java
+++ src/main/java/net/zytolga/Jerry.java
@@ -2,31 +2,17 @@ package net.zytolga;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.components.actionrow.ActionRow;
-import net.dv8tion.jda.api.components.buttons.Button;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.IntegrationType;
 import net.dv8tion.jda.api.interactions.InteractionContextType;
-import net.dv8tion.jda.api.interactions.InteractionHook;
-import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
-import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.EnumSet;
-import java.util.concurrent.TimeUnit;
-
-import static net.dv8tion.jda.api.interactions.commands.OptionType.*;
 
 public class Jerry extends ListenerAdapter {
     private static final Logger logger = LoggerFactory.getLogger(Jerry.class);
@@ -44,11 +30,18 @@ public class Jerry extends ListenerAdapter {
 
         CommandListUpdateAction commands = jda.updateCommands();
 
-        commands.addCommands(Commands.slash("jerry", "jerry :)")
-                .setContexts(InteractionContextType.ALL)
-                .setIntegrationTypes(IntegrationType.ALL));
+        try {
+            //noinspection ResultOfMethodCallIgnored
+            commands.addCommands(Commands.slash("jerry", "jerry :)")
+                    .setContexts(InteractionContextType.ALL)
+                    .setIntegrationTypes(IntegrationType.ALL));
 
-        commands.queue();
+            commands.queue();
+        } catch (IllegalArgumentException e) {
+            logger.error("Error adding commands. Null or more than 100 commands, 15 user context commands, or 15 message context commands, are provided.", e);
+        } catch (Exception e) {
+            logger.error("Error adding commands", e);
+        }
     }
 
     @Override
@@ -57,6 +50,7 @@ public class Jerry extends ListenerAdapter {
         if (event.getGuild() == null) {
             return;
         }
+        //noinspection SwitchStatementWithTooFewBranches
         switch (event.getName()) {
             case "jerry":
                 say(event, "https://klipy.com/gifs/creo-creomusic-1");
